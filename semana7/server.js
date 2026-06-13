eb5af51d2b2d722403d109ca7404bfad92ae7499
otaloraluzestefannia-web <otaloraluzestefannia@gmail.com>
2026-06-13 15:35:30 +0000

    semana7

--- semana7/server.js
+++ semana7/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-
+app.get('/', (req, res) => {
+  res.send('Servidor activo');
+});
 let reportes = [];
 
 app.get('/reportes', (req, res) => {
